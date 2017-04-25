6560ba7eb5d251ffc243d6402c3f00653364905f
KonnyAmaya <konyliz_ab@hotmail.com.ar>
2017-04-25 02:21:41 -0600

    menu

---
+++ build/CMakeFiles/solido.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solido.dir/main.cpp.o"
+  "solido.pdb"
+  "solido"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solido.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
